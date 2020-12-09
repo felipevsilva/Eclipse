509c2dfec3314b44e52970a6fa525b3f15fec0a6
Felipe Silva <52672119+felipevsilva@users.noreply.github.com>
2020-12-09 19:08:34 -0300

    Adicionei arquivos via upload

---
+++ ProjIMC/src/estudo1.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class estudo1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.print("Digite o nome: ");
+		String nome = sc.next();
+		System.out.print("Digite idade: ");
+		int idade = sc.nextInt();
+		System.out.print("Digite peso: ");
+		float peso = sc.nextFloat();
+		System.out.println("Digite sua altura: ");
+		float alt = sc.nextFloat();
+		
+		float imc = peso/(alt * alt);
+		float ResImc = imc;
+		
+		System.out.println(nome);
+		System.out.println(idade);	
+		System.out.println(peso);
+		System.out.println(alt);
+		System.out.println(ResImc);
+		
+		if (ResImc > 18.5 && ResImc <24.9){
+			
+			System.out.println("Peso normal");
+			
+		} else {
+			
+			System.out.println("Fora do peso ideal");
+		}
+		
+		
+	
+			
+		
+		
+	}
+	
+	
+
+}
